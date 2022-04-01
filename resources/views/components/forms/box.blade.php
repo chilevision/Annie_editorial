f01eed200853d5e489b494e8083fd0f8c082112f
Andreas Andersson <andreas@amedia.nu>
2022-04-01 15:38:13 +0200

    fix for undefined error

--- resources/views/components/forms/box.blade.php
+++ resources/views/components/forms/box.blade.php
@@ -1,12 +1,12 @@
 <div class="form-check {{ $wrapClass }}">
 @if (!isset($snappy) || $snappy != '')
     <input type="checkbox" value="{{ $value ?? '' }}" name="{{ $name }}" class="form-check-input {{ $inputClass ?? '' }}" id="input-{{ $name }}" 
-        @if (!isset($wires) || $wires != '') wire:model="{{ $wires }}" @endif
+        @if (!isset($wires) || $wires != '') wire:model="{{ $wires ?? ''}}" @endif
         @if (!isset($checked) || $checked != '') checked @endif
     />
 @else
     <input type="checkbox" value="{{ $value ?? '' }}" name="{{ $name }}" class="form-check-input {{ $inputClass ?? '' }}" id="input-{{ $name }}" 
-        @if (!isset($wires) || $wires != '') wire:model.lazy="{{ $wires }}" @endif
+        @if (!isset($wires) || $wires != '') wire:model.lazy="{{ $wires ?? ''}}" @endif
         @if (!isset($checked) || $checked != '') checked @endif
     />
 @endif
