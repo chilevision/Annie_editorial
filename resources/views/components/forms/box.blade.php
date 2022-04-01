59b92db5b575a8e782d6197ac50738f57bc9e1a9
Andreas Andersson <andreas@amedia.nu>
2022-04-01 15:36:09 +0200

    fix for undifined error

--- resources/views/components/forms/box.blade.php
+++ resources/views/components/forms/box.blade.php
@@ -1,15 +1,15 @@
 <div class="form-check {{ $wrapClass }}">
 @if (!isset($snappy) || $snappy != '')
-    <input type="checkbox" value="{{ $value }}" name="{{ $name }}" class="form-check-input {{ $inputClass }}" id="input-{{ $name }}" 
-        @if ($wires != '') wire:model="{{ $wires }}" @endif
-        @if ($checked != '') checked @endif
+    <input type="checkbox" value="{{ $value ?? '' }}" name="{{ $name }}" class="form-check-input {{ $inputClass }}" id="input-{{ $name }}" 
+        @if (!isset($wires) || $wires != '') wire:model="{{ $wires }}" @endif
+        @if (!isset($checked) || $checked != '') checked @endif
     />
 @else
-    <input type="checkbox" value="{{ $value }}" name="{{ $name }}" class="form-check-input {{ $inputClass }}" id="input-{{ $name }}" 
-        @if ($wires != '') wire:model.lazy="{{ $wires }}" @endif
-        @if ($checked != '') checked @endif
+    <input type="checkbox" value="{{ $value ?? '' }}" name="{{ $name }}" class="form-check-input {{ $inputClass }}" id="input-{{ $name }}" 
+        @if (!isset($wires) || $wires != '') wire:model.lazy="{{ $wires }}" @endif
+        @if (!isset($checked) || $checked != '') checked @endif
     />
 @endif
     <label class="form-check-label" for="input-{{ $name }}">{{ __($label) }}</label>
-    @error($name) <span class="text-danger">{{ $message }}</span> @enderror
+    @error($name) <span class="text-danger">{{ $message ?? '' }}</span> @enderror
 </div>
